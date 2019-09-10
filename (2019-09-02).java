6893dee15d32e5efdb68c5c5e3e438684c97542f
Sh031224 <51149996+Sh031224@users.noreply.github.com>
2019-09-10 15:37:15 +0900

    Add files via upload

---
+++ (2019-09-02).java
@@ -0,0 +1,52 @@
+
+public class Hello {
+
+	public static void main(String[] args) {
+//		int n1 = ((25 + 5) + (36 / 4) - 72) * 5;
+//        int n2 = ((25 * 5) + (36 - 4) + 71) / 4;
+//        int n3 = (128 / 4) * 2;
+//
+//        boolean isBig = (n1 > n2) && (n2 > n3);
+//        System.out.println(isBig);
+		
+		
+//		byte n1 = 5; //00000101
+//		byte n2 = 3; //00000011
+//		byte n3 = -1; //11111111
+//		
+//		byte r1 = (byte)(n1 & n2);
+//		byte r2 = (byte)(n1 | n2);
+//		byte r3 = (byte)(n1 ^ n2);
+//		byte r4 = (byte)(~n3);
+//		
+//		System.out.println(r1);
+//		System.out.println(r2);
+//		System.out.println(r3);
+//		System.out.println(r4);
+		
+		
+//		byte num;
+//		num = 2;
+//		
+//		System.out.print(((byte)num << 1) + " ");
+//		System.out.print(((byte)num << 2) + " ");
+//		System.out.print(((byte)num << 3) + " " + '\n');
+//		
+//		num = 8;
+//		System.out.print(((byte)num << 1) + " ");
+//		System.out.print(((byte)num << 2) + " ");
+//		System.out.print(((byte)num << 3) + " " + '\n');
+//		
+//		num = -8;
+//		System.out.print(((byte)num << 1) + " ");
+//		System.out.print(((byte)num << 2) + " ");
+//		System.out.print(((byte)num << 3) + " " + '\n');
+		
+		
+		byte num = 7;
+		System.out.print((byte)-num);
+		
+		int num2 = 15678;
+		
+	}
+}
